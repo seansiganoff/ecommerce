07b7f72fb970372cf097536f17a380ef4ea89314
Sean Siganoff <nationwideclassics@yahoo.com>
2022-06-21 18:15:37 -0400

    updated the fonts and text

--- src/Components/nav/Nav.js
+++ src/Components/nav/Nav.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import './style.css'
+import './nav.css'
 import { Link, useMatch, useResolvedPath } from 'react-router-dom';
 
 
--- src/Components/watches/Showcase.js
+++ src/Components/watches/Showcase.js
@@ -5,31 +5,30 @@ import { useLocation } from 'react-router-dom'
 export default function WatchView() {
   const location = useLocation();
   const {from} = location.state;
-  let watch = '';
+  const watch = from;
   
-  if(from.model.includes('DAYTONA')) watch = 'DAYTONA'
-  else if(from.model.includes('PRESIDENTIAL')) watch = 'PRESIDENTIAL'
-  else if(from.model.includes('SUBMARINER')) watch ='SUBMARINER'
-  else if(from.model.includes('GMT')) watch = 'GMT'
-  else if(from.model.includes('DATEJUST TWO')) watch = 'DATEJUST TWO';
   
-  const datejust2_heading = 'THE OYSTER PERPETUAL DATEJUST IS THE EPITOME OF THE CLASSIC ROLEX WATCH.';
-  const datejust2_description = "Created in 1945, the Datejust was the first self-winding waterproof chronometer wristwatch to display the date in a window at 3 o'clock on the dial hence its name. With its timeless aesthetics, functions and rich history, the Datejust is a watchmaking icon and one of the brands most recognizable watches."
+  
   
   return (
     <div className='showcase-container'>
       <div className='watch-container'>
         <div className='showcase-left-side'>
-          <h1>{watch}</h1>
+          <h1>{watch.model} </h1>
+          <br />
+          <p>{watch.heading}</p>
+          <br />
+          <div className='price'>{watch.price}</div>
         </div>
         <div className='showcase-right-side'>
-          <img alt='Dayjust2' src={from.url} />
+          <img alt='Dayjust2' src={watch.url} />
         </div>
       </div>
-      <div className='description-div'>
-        <div className='watch-header-div' >{datejust2_heading}</div>
-        <div className='watch-description-div'>{datejust2_description}</div>
+      <div className='showcase-description-div'>
+        <div className='watch-description-div' >{watch.description}</div>
+        <div className='watch-info-div'>{watch.info}</div>
       </div>
+      
     </div>
   )
 }
